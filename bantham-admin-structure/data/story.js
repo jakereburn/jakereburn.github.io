e3506a50fde8689b667a755357bf407e81d92f0f
Jacob Reburn <jacobreburn@Jacobs-Work-MacBook-Pro.local>
2022-09-06 16:11:35 +0100

    bantham-admin-strucutre demo

---
+++ bantham-admin-structure/data/story.js
@@ -0,0 +1,686 @@
+var story = {
+ "docName": "bantham-strucutre",
+ "docPath": "P_P_P",
+ "docVersion": "V_V_V",
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "commentsURL": "V_V_C",
+ "hasRetina": true,
+ "serverToolsPath": "",
+ "fontSizeFormat": 0,
+ "fileType": "png",
+ "disableHotspots": false,
+ "zoomEnabled": true,
+ "title": "Bantham Strucutre",
+ "layersExist": true,
+ "centerContent": false,
+ "highlightLinks": false,
+ "pages": [
+  {
+   "id": "5DF40F88-84EF-4235-A0F1-60529414D96D",
+   "groupID": "C4FB115B-0629-4736-90AD-DD70A83728AB",
+   "index": 0,
+   "image": "desktop_hd.png",
+   "image2x": "desktop_hd@2x.png",
+   "width": 1440,
+   "height": 1024,
+   "x": -204,
+   "y": -197,
+   "title": "Desktop HD",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "D687315D-6212-4D23-AFD8-60743B64FF10",
+   "groupID": "C4FB115B-0629-4736-90AD-DD70A83728AB",
+   "index": 1,
+   "image": "mobile.png",
+   "image2x": "mobile@2x.png",
+   "width": 320,
+   "height": 600,
+   "x": 1336,
+   "y": -197,
+   "title": "Mobile",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "E0C781CC-A0E5-491B-A6CB-1BDF469E9D2D",
+   "groupID": "C4FB115B-0629-4736-90AD-DD70A83728AB",
+   "index": 2,
+   "image": "tablet.png",
+   "image2x": "tablet@2x.png",
+   "width": 768,
+   "height": 1024,
+   "x": 1756,
+   "y": -197,
+   "title": "Tablet",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "2FDE0424-967A-4793-B401-7162AF3D6317",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 3,
+   "image": "home.png",
+   "image2x": "home@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": -207,
+   "y": -167,
+   "title": "Home",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 10,
+      "y": 289,
+      "width": 178,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 6,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 261,
+      "width": 178,
+      "height": 28
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 221,
+      "width": 178,
+      "height": 38
+     },
+     "isParentFixed": false,
+     "page": 4,
+     "index": 2
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 181,
+      "width": 178,
+      "height": 38
+     },
+     "isParentFixed": false,
+     "page": 3,
+     "index": 3
+    }
+   ]
+  },
+  {
+   "id": "3F283E6E-4E32-4E81-9983-9B3DD9EA7BD8",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 4,
+   "image": "client_management.png",
+   "image2x": "client_management@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": -207,
+   "y": 904,
+   "title": "Client Management",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 239,
+      "y": 576,
+      "width": 110,
+      "height": 53
+     },
+     "isParentFixed": false,
+     "page": 14,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 255,
+      "y": 203,
+      "width": 257,
+      "height": 353
+     },
+     "isParentFixed": false,
+     "page": 7,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 289,
+      "width": 178,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 6,
+     "index": 2
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 261,
+      "width": 178,
+      "height": 28
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 3
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 221,
+      "width": 178,
+      "height": 38
+     },
+     "isParentFixed": false,
+     "page": 4,
+     "index": 4
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 181,
+      "width": 178,
+      "height": 38
+     },
+     "isParentFixed": false,
+     "page": 3,
+     "index": 5
+    }
+   ]
+  },
+  {
+   "id": "4026C7D1-475E-44BB-8BFD-A6EFD1B3416E",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 5,
+   "image": "profile.png",
+   "image2x": "profile@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": -207,
+   "y": 2028,
+   "title": "Profile",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 10,
+      "y": 289,
+      "width": 178,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 6,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 261,
+      "width": 178,
+      "height": 28
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 221,
+      "width": 178,
+      "height": 38
+     },
+     "isParentFixed": false,
+     "page": 4,
+     "index": 2
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 181,
+      "width": 178,
+      "height": 38
+     },
+     "isParentFixed": false,
+     "page": 3,
+     "index": 3
+    }
+   ]
+  },
+  {
+   "id": "234DEAC7-B5E8-45E1-BA3B-EB7DBEBF053C",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 6,
+   "image": "user_management.png",
+   "image2x": "user_management@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": -207,
+   "y": 3152,
+   "title": "User Management",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 10,
+      "y": 289,
+      "width": 178,
+      "height": 32
+     },
+     "isParentFixed": false,
+     "page": 6,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 261,
+      "width": 178,
+      "height": 28
+     },
+     "isParentFixed": false,
+     "page": 5,
+     "index": 1
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 221,
+      "width": 178,
+      "height": 38
+     },
+     "isParentFixed": false,
+     "page": 4,
+     "index": 2
+    },
+    {
+     "rect": {
+      "x": 10,
+      "y": 181,
+      "width": 178,
+      "height": 38
+     },
+     "isParentFixed": false,
+     "page": 3,
+     "index": 3
+    }
+   ]
+  },
+  {
+   "id": "BC3824DB-40D2-4B49-9D95-620220A5C7DC",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 7,
+   "image": "client_management_details.png",
+   "image2x": "client_management_details@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 1333,
+   "y": 904,
+   "title": "Client Management - Details",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 297.5999999999999,
+      "y": 579,
+      "width": 45,
+      "height": 45
+     },
+     "isParentFixed": false,
+     "page": 11,
+     "index": 0
+    },
+    {
+     "rect": {
+      "x": 342.5999999999999,
+      "y": 579,
+      "width": 85,
+      "height": 44
+     },
+     "isParentFixed": false,
+     "page": 3,
+     "index": 1
+    }
+   ]
+  },
+  {
+   "id": "7C43AE6B-BD98-47B1-82A1-B89C386F47BA",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 8,
+   "image": "profile_upload_signature.png",
+   "image2x": "profile_upload_signature@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 1333,
+   "y": 2028,
+   "title": "Profile - Upload Signature",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "9BAA5060-24D8-4371-B29A-EB2973B00F2D",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 9,
+   "image": "user_management_selected_(permissions).png",
+   "image2x": "user_management_selected_(permissions)@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 1333,
+   "y": 3152,
+   "title": "User Management - Selected (Permissions)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "C1BE2F67-0E2B-43BF-8521-AD547CB1EC88",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 10,
+   "image": "user_management_new_(permissions).png",
+   "image2x": "user_management_new_(permissions)@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 1333,
+   "y": 4276,
+   "title": "User Management - New (Permissions)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "2AB2B651-3E42-4C95-A376-019E43014201",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 11,
+   "image": "client_management_details(1).png",
+   "image2x": "client_management_details(1)@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 2873,
+   "y": 904,
+   "title": "Client Management - Details(1)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "rect": {
+      "x": 296.5999999999999,
+      "y": 579,
+      "width": 84,
+      "height": 43
+     },
+     "isParentFixed": false,
+     "page": 3,
+     "index": 0
+    }
+   ]
+  },
+  {
+   "id": "6679C62B-F55C-4766-8FAC-97A73E831DBB",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 12,
+   "image": "user_management_selected_(devices).png",
+   "image2x": "user_management_selected_(devices)@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 2873,
+   "y": 3152,
+   "title": "User Management - Selected (Devices)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "8561BD3D-F380-49C0-B5D2-AA28E3A040C9",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 13,
+   "image": "user_management_new_(devices).png",
+   "image2x": "user_management_new_(devices)@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 2873,
+   "y": 4276,
+   "title": "User Management - New (Devices)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "3765ACAD-7392-4C2B-9766-240A0A158832",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 14,
+   "image": "client_management_new.png",
+   "image2x": "client_management_new@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 4413,
+   "y": 904,
+   "title": "Client Management - New",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "EA8CB4F2-7AC1-4563-AD0C-5AF4600B0ED4",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 15,
+   "image": "user_management_selected_(groups).png",
+   "image2x": "user_management_selected_(groups)@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 4413,
+   "y": 3152,
+   "title": "User Management - Selected (Groups)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "F4881435-8EFC-4C06-A2FD-DC2AD3E10CA8",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 16,
+   "image": "user_management_new_(groups).png",
+   "image2x": "user_management_new_(groups)@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 4413,
+   "y": 4276,
+   "title": "User Management - New (Groups)",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  },
+  {
+   "id": "DFC781A3-EB40-45AD-A9C0-C5E61D8E2B37",
+   "groupID": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "index": 17,
+   "image": "user_management_new_upload_modal.png",
+   "image2x": "user_management_new_upload_modal@2x.png",
+   "width": 1440,
+   "height": 830,
+   "x": 5953,
+   "y": 4276,
+   "title": "User Management - New - Upload Modal",
+   "transAnimType": 0,
+   "layout": {
+    "offset": 0,
+    "totalWidth": 960,
+    "numberOfColumns": 12,
+    "columnWidth": 60,
+    "gutterWidth": 20
+   },
+   "type": "regular",
+   "fixedPanels": [],
+   "links": []
+  }
+ ],
+ "groups": [
+  {
+   "id": "C4FB115B-0629-4736-90AD-DD70A83728AB",
+   "name": "2019 Design"
+  },
+  {
+   "id": "5A1CE267-2E3D-4C5C-98B7-AEC168087973",
+   "name": "Existing Admin Structure"
+  }
+ ],
+ "experimentalExisting": false,
+ "startPageIndex": 3,
+ "totalImages": 18
+}
